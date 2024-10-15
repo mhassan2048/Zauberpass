fde8b5dafb98d51ccc8ab911d66c1d681f532914
Mehedi Hassan <mehedihassan2048@gmail.com>
2024-10-15 13:11:28 -0400

    Updated spadl data.

--- .ipynb_checkpoints/Zauberpass-checkpoint.py
+++ .ipynb_checkpoints/Zauberpass-checkpoint.py
@@ -408,7 +408,9 @@ def draw_pass_clusters(passes, cluster_info, team, game_info, player, data_type_
 def load_data(tournament):
     data_sources = {
         "La Liga 2024-25": "https://drive.google.com/uc?export=download&id=1p4Bh_DaE-LKuoPKr_7Blz2uDXQSLPVx-",
-        "La Liga 2024-25 Spadl": "https://drive.google.com/uc?export=download&id=1V1jYBaV-ONKnIqh1iJyjioZuiw-evF62"
+        "La Liga 2024-25 Spadl": "https://drive.google.com/uc?export=download&id=1V1jYBaV-ONKnIqh1iJyjioZuiw-evF62",
+        "La Liga 2020-21": "https://drive.google.com/uc?export=download&id=1IaT9rsGFMlC_-0GDdx9qddBgDE5wq8Ey",
+        "La Liga 2020-21 Spadl": "https://drive.google.com/uc?export=download&id=1LYUTCp7rdjSVzuDmSx7FVupFKugwIfwh"
     }
     url = data_sources[tournament]
     
@@ -426,7 +428,8 @@ def load_data(tournament):
         return None
 def load_spadl_data(tournament):
     spadl_data_sources = {
-        "La Liga 2024-25": "https://drive.google.com/uc?export=download&id=1V1jYBaV-ONKnIqh1iJyjioZuiw-evF62"
+        "La Liga 2024-25": "https://drive.google.com/uc?export=download&id=1V1jYBaV-ONKnIqh1iJyjioZuiw-evF62",
+        "La Liga 2020-21": "https://drive.google.com/uc?export=download&id=1LYUTCp7rdjSVzuDmSx7FVupFKugwIfwh"
     }
     url = spadl_data_sources[tournament]
     df = pd.read_csv(url)
